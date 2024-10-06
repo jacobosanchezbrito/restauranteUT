3d80da7147d980d2bb962175490149d5388e20a9
jacobosanchezbrito <sbjacobo4@gmail.com>
2024-10-06 17:53:15 -0500

    Primer commit XD

---
+++ admin/includes/footer.php
@@ -0,0 +1,33 @@
+</div>
+
+</div>
+
+<footer class="sticky-footer bg-white">
+</footer>
+
+</div>
+
+
+</div>
+
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+
+
+<script src="../assets/js/jquery-3.6.0.min.js"></script>
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+
+<script src="../assets/js/jquery.easing.min.js"></script>
+
+
+<script src="../assets/js/sb-admin-2.min.js"></script>
+<script src="../assets/js/all.min.js"></script>
+<script src="../assets/js/scripts.js"></script>
+
+
+</body>
+
+</html>
